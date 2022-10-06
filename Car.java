e6f977778751fdc8e2784f2e6ad8aa3732b592df
SivaramaKrishnan-KR <115095960+SivaramaKrishnan-KR@users.noreply.github.com>
2022-10-06 23:14:57 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int tripMeter = 123456;
+		byte seat =5;
+		short year = 2021;
+		long kilometer = 1234567890L;
+		char tyre = '4';
+		float tankCapacity = 350.999F;
+		double chaseNum = 1234569.9456123;
+		boolean diesel= true;
+		
+		String brand = "Skoda";
+
+	}
+
+}
